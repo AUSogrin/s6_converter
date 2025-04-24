d0e9ea1149cf13be91317ac41469767eb8fa031b
Andrey Sogrin <sau1992@mail.ru>
2025-04-25 02:58:56 +0300

    first iteration

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/server"
+)
+
 func main() {
 
+	logger := log.New(os.Stdout, "Server: ", log.Ldate|log.Ltime|log.Lshortfile)
+
+	logger.Println("Initializing server ...")
+	srv := server.New(logger)
+
+	logger.Println("Server running on port :8080")
+	if err := srv.Server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		logger.Fatalf("Server error: %v", err)
+	}
 }
--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,37 @@
 package service
+
+import (
+	"errors"
+	"strings"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/pkg/morse"
+)
+
+func Converter(received string) (string, error) {
+	received = strings.TrimSpace(received)
+	if received == "" {
+		return "", errors.New("empty input")
+	}
+
+	if isMorseCode(received) {
+		textResult := morse.ToText(received)
+		if !strings.Contains(textResult, "No encoding for") {
+			return textResult, nil
+		}
+	}
+	morseResult := morse.ToMorse(strings.ToUpper(received))
+	if morseResult == "" || strings.Contains(morseResult, "No encoding for") {
+		return "", errors.New("invalid input")
+	}
+
+	return morseResult, nil
+}
+
+func isMorseCode(s string) bool {
+	for _, r := range s {
+		if r != '.' && r != '-' && r != ' ' && r != '/' {
+			return false
+		}
+	}
+	return true
+}
